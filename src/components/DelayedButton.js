70b2a29ca8b4cddb5b7585e8509f8d5cc9c9e11d
Liz P <parisielizabeth@gmail.com>
2021-01-25 04:23:14 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -5,6 +5,14 @@ export default class DelayedButton extends Component {
   
   handleClick = (event) => {
     event.persist()
-    setTimeout((this.props.delay))
+    setTimeout(() => {
+      this.props.onDelayedClick(event);
+    }, this.props.delay);
+  }
+  
+  render() {
+    return(
+      
+      )
   }
 }
