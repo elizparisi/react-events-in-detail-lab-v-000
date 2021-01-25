d060a1b8cdedb52d2ae08acd1fe7820e5ed8b468
Liz P <parisielizabeth@gmail.com>
2021-01-25 04:14:35 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -9,7 +9,7 @@ export default class CoordinatesButton extends Component {
   
   render(){
     return (
-      <button onClick={this.handleClick}> </button>
+      <button onClick={this.handleClick}>Coordinates Button</button>
     )
   }
   
