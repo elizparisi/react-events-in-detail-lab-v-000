e7cd75749df3b5c0c87986f72d7da3e07a94bb52
Liz P <parisielizabeth@gmail.com>
2021-01-25 04:13:58 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -7,6 +7,10 @@ export default class CoordinatesButton extends Component {
     this.props.onReceiveCoordinates{[event.clientX, event.clientY]
   }
   
-  
+  render(){
+    return (
+      <button> </button>
+    )
+  }
   
 }
